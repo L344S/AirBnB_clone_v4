b330f82bd3e6e26862f96ee9f12b5f5c92f48227
L344S <145486211+L344S@users.noreply.github.com>
2024-05-30 12:57:46 +0200

    Add (T3) jQuery functionality for selecting amenities

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  const url = 'http://localhost:5001/api/v1/status/';
+  $.get(url, (data, status) => {
+    if (status === 'success' && data.status === 'OK') {
+      $('#api_status').addClass('available');
+    }
+  });
+  const amenitiesSelect = [];
+  $('input:checkbox').change(function () {
+    if ($(this).is(':checked')) {
+      amenitiesSelect.push($(this).parent().attr('data-name'));
+    } else {
+      const index = amenitiesSelect.indexOf(($(this).parent().attr('data-name')));
+      if (index > -1) {
+        amenitiesSelect.splice(index, 1);
+      }
+    }
+    $('.amenities h4').text(amenitiesSelect.join(', '));
+  });
+});
